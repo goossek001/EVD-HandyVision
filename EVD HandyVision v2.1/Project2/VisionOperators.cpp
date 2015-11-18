8cfda8dc0d2238abe1d0f6ea9f49a6f4ea4fca5a
goossek001 <kay.goossen@gmail.com>
2015-11-18 14:41:11 +0100

    Recreated Mat & Point & Color

---
+++ EVD HandyVision v2.1/Project2/VisionOperators.cpp
@@ -0,0 +1,73 @@
+#include "VisionOperators.h"
+#include <algorithm>
+
+Point::Point(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
+Color::Color(int R, int G, int B, int A) {
+	mR = R;
+	mG = G;
+	mB = B;
+	mA = A;
+}
+
+int bytesPerPixel(ImageType type) {
+	switch (type) {
+	default:
+		throw "PixelSize(type) is missing the implementation for type '???'";
+	case IM_8UC1:
+		return 1;
+	case IM_8UC3:
+		return 3;
+	}
+}
+
+
+Mat::Mat(Point size, ImageType type) {
+	rows = size.x;
+	cols = size.y;
+	this->type = type;
+
+	int length = bytesPerPixel(type) * rows * cols;
+	data = new unsigned char[length];
+	std::fill(data, data + length, 0);
+}
+
+Mat::~Mat() {
+	delete[] data;
+}
+
+Color Mat::get(int i, int j) {
+	int pixelSize = bytesPerPixel(type);
+	unsigned char* loc = data + pixelSize * j + pixelSize * i * cols;
+	Color* color = reinterpret_cast<Color*>(loc);
+
+	return *color;
+}
+Color Mat::get(Point index) {
+	return get(index.y, index.x);
+}
+
+void Mat::set(int i, int j, Color color) {
+	int pixelSize = bytesPerPixel(type);
+	unsigned char* loc = data + pixelSize * j + pixelSize * i * cols;
+
+	*loc = color.R();
+	if (pixelSize > 1) {
+		++loc;
+		*loc = color.G();
+		if (pixelSize > 2) {
+			++loc;
+			*loc = color.B();
+			if (pixelSize > 3) {
+				++loc;
+				*loc = color.A();
+			}
+		}
+	}
+}
+void Mat::set(Point index, Color color) {
+	set(index.y, index.x, color);
+}
--- EVD HandyVision v2.1/Project2/VisionOperators.h
+++ EVD HandyVision v2.1/Project2/VisionOperators.h
@@ -1,1 +1,42 @@
 #pragma once
+
+struct Point {
+	int x, y;
+
+	Point(int x, int y);
+};
+
+struct Color {
+	unsigned char mR, mG, mB, mA;
+
+	Color(int R, int G = 0, int B = 0, int A = 0);
+
+	inline int R() { return (int)mR; }
+	inline int G() { return (int)mG; }
+	inline int B() { return (int)mB; }
+	inline int A() { return (int)mA; }
+};
+
+enum ImageType {
+	IM_8UC1,
+	IM_8UC3
+};
+
+
+int bytesPerPixel(ImageType type);
+
+struct Mat {
+public:
+	unsigned char* data;
+	unsigned int rows, cols;
+	ImageType type;
+
+	Mat(Point size, ImageType type);
+	~Mat();
+
+	Color get(int i, int j);
+	Color get(Point index);
+
+	void set(int i, int j, Color color);
+	void set(Point index, Color color);
+};
